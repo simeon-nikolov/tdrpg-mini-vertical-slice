de419a457e197126528dbb30a0847cd71d6eaa58
Simeon Nikolov <simeon.v.nikolov@gmail.com>
2025-05-05 00:41:28 +0300

    Use this.

--- Assets/Scripts/Combat/UnitBehavior.cs
+++ Assets/Scripts/Combat/UnitBehavior.cs
@@ -5,50 +5,48 @@ public class UnitBehavior : MonoBehaviour
     public float moveSpeed = 2f;
 
     private AutoAttacker autoAttacker;
-    private Transform target;
     private Animator animator;
 
     private void Start()
     {
-        animator = GetComponent<Animator>();
-        autoAttacker = GetComponent<AutoAttacker>();
+        this.animator = GetComponent<Animator>();
+        this.autoAttacker = GetComponent<AutoAttacker>();
     }
 
     private void Update()
     {
-        if (autoAttacker == null || autoAttacker.target == null)
+        if (this.autoAttacker == null || this.autoAttacker.target == null)
         {
-            StopWalking();
+            this.StopWalking();
             return;
         }
 
-        target = autoAttacker.target;
-        float distance = Vector3.Distance(transform.position, target.position);
+        float distance = Vector3.Distance(this.transform.position, this.autoAttacker.target.position);
 
-        if (distance > autoAttacker.attackRange)
+        if (distance > this.autoAttacker.attackRange)
         {
-            MoveTowardTarget(distance);
+            this.MoveTowardTarget(distance);
         }
         else
         {
-            StopWalking();
+            this.StopWalking();
         }
     }
 
     private void MoveTowardTarget(float distance)
     {
-        StartWalking();
-        Vector3 direction = (target.position - transform.position).normalized;
-        transform.position += moveSpeed * Time.deltaTime * direction;
+        this.StartWalking();
+        Vector3 direction = (this.autoAttacker.target.position - this.transform.position).normalized;
+        this.transform.position += this.moveSpeed * Time.deltaTime * direction;
     }
 
     void StartWalking()
     {
-        animator.SetBool(AnimatorConstants.WALKING_PARAM, true);
+        this.animator.SetBool(AnimatorConstants.WALKING_PARAM, true);
     }
 
     void StopWalking()
     {
-        animator.SetBool(AnimatorConstants.WALKING_PARAM, false);
+        this.animator.SetBool(AnimatorConstants.WALKING_PARAM, false);
     }
 }
